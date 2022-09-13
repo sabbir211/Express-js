a2a9c369a05c8b2fb79eb8153ea40a0ed15225a0
sabbir <sabbir21122@gmail.com>
2022-09-13 17:28:42 +0600

    try10

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express=require("express")
-const app=express()
-const port= process.env.PORT || 5000
-const cors=require("cors")
-const userRoute=require("./Routes/v1/User.route")
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require("cors")
+const userRoute = require("./Routes/v1/User.route")
 
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/v1/user',userRoute)
-app.get('/',(req,res)=>{
+app.use('/api/v1/user', userRoute)
+app.get('/', (req, res) => {
     res.send('Server is running well')
 })
-app.get("/api/v2/text",(req,res)=>{
+app.get("/api/v2/text", (req, res) => {
     fs.readFile('userData.json', "utf-8", (err, data) => {
         if (err) {
-
-            res.end()
+            console.log(err)
+            res.send(err)
         }
         else {
             const users = JSON.parse(data)
@@ -24,15 +24,15 @@ app.get("/api/v2/text",(req,res)=>{
         }
     })
 })
-app.all('*',(req,res)=>{
-res.send("NOT FOUND WHAT YOU ARE LOOKING FOR")
+app.all('*', (req, res) => {
+    res.send("NOT FOUND WHAT YOU ARE LOOKING FOR")
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server is well")
 })
 process.on("unhandledRejection", (error) => {
     console.log(error.name, error.message);
     app.close(() => {
-      process.exit(1);
+        process.exit(1);
     });
-  });
+});
