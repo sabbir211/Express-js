b1a0925562c13c6f09176c4717858f0b9914354f
sabbir <sabbir21122@gmail.com>
2022-09-14 12:10:37 +0600

    replace to heroku

--- index.js
+++ index.js
@@ -12,19 +12,7 @@ app.use('/api/v1/user', userRoute)
 app.get('/', (req, res) => {
     res.send('Server is running well')
 })
-app.get("/api/v2/text", (req, res) => {
-    fs.readFile('./userData.json', "utf-8", (err, data) => {
-        if (err) {
-            console.log(err,"i am from if")
-            res.end()
-        }
-        else {
-            const users = JSON.parse(data)
-            console.log("hey i am from else")
-            res.send(users)
-        }
-    })
-})
+
 app.all('*', (req, res) => {
     res.send("NOT FOUND WHAT YOU ARE LOOKING FOR")
 })
